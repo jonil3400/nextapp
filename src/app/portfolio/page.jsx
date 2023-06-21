097b7700df211bd0c63a5370f5251616197c977b
Jonil <jonilsarmiento4@gmail.com>
2023-06-21 17:05:41 +0800

    3rd Commit

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -7,13 +7,13 @@ const Portfolio = () => {
     <div className={styles.container}>
       <h1 className={styles.selectTitle}>Choose a gallery</h1>
       <div className={styles.items}>
-        <Link href="portfolio/illustrations" className={styles.item}>
+        <Link href="/portfolio/illustrations" className={styles.item}>
           <span className={styles.title}>Illustrations</span>
         </Link>
-        <Link href="portfolio/websites" className={styles.item}>
+        <Link href="/portfolio/websites" className={styles.item}>
           <span className={styles.title}>Websites</span>
         </Link>
-        <Link href="portfolio/applications" className={styles.item}>
+        <Link href="/portfolio/applications" className={styles.item}>
           <span className={styles.title}>Applications</span>
         </Link>
       </div>
