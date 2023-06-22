a28fbeb979b11dad4a4ae80594ba1ec6c65bf92d
Jonil <jonilsarmiento4@gmail.com>
2023-06-22 17:39:12 +0800

    auth

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import styles from "./page.module.css";
 
 const LogIn = () => {
-  return <div>LogIn</div>;
+  return (
+    <div>
+      <button>Login With Google</button>
+    </div>
+  );
 };
 
 export default LogIn;
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import styles from "./page.module.css";
+import useSWR from "swr";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 const DashBoard = () => {
-  return <div>DashBoard</div>;
+  const session = useSession();
+
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data, mutate, error, isLoading } = useSWR(
+    `/api/posts?username=${session?.data?.user.name}`,
+    fetcher
+  );
+  return <div className={styles.container}>DashBoard</div>;
 };
 
 export default DashBoard;
