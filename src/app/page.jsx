53d67c65ed52079267c6fb9f5848fb1880e5f078
Jonil <jonilsarmiento4@gmail.com>
2023-06-22 17:38:27 +0800

    Commit Auth

---
+++ src/app/page.jsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Hero from "public/hero.png";
+import Button from "@/components/Button/Button";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Welcome to the Home Page</h1>
+        <p className={styles.desc}>
+          lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum
+          dolor sit amet
+        </p>
+        <Button url="/portfolio" text="See Our Works" />
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt="Hero" priority className={styles.img} />
+      </div>
+    </div>
+  );
+}
